a70bb7a13fbd399423d5ab0a745cefbfbb2475da
devzhong <devzhong@163.com>
2019-02-18 09:34:24 +0800

    Initial commit

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Description:
+ * @Author: devzhong
+ * @Date: 2019/2/18 9:30
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
